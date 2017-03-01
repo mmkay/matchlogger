cced5ecd350e0b67826057e8d4bb7cc79d73a0a3
Mateusz Kulewicz <mateusz.kulewicz@gmail.com>
2017-03-01 21:05:34 +0100

    #1 App starts up locally

---
+++ src/main/java/pl/mmkay/MatchloggerApplication.java
@@ -0,0 +1,12 @@
+package pl.mmkay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchloggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchloggerApplication.class, args);
+	}
+}
