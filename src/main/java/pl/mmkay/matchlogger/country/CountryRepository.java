7d3f0c3ee4592b669fedcec1daba18270a54b164
Mateusz Kulewicz <mateusz.kulewicz@gmail.com>
2017-03-25 09:19:32 +0100

    #11 countries entity for team adding

---
+++ src/main/java/pl/mmkay/matchlogger/country/CountryRepository.java
@@ -0,0 +1,8 @@
+package pl.mmkay.matchlogger.country;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CountryRepository extends PagingAndSortingRepository<Country, Integer> {
+}
